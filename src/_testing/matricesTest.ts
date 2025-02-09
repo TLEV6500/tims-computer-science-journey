8ba2f8dc931d7e292d1bd5387922db5e3b27a7a4
Tim Leobert Villacin <timvillacin@gmail.com>
2025-02-09 23:03:39 +0800

    * Using ElementaryMatrixRowOperations.main

--- src/_testing/matricesTest.ts
+++ src/_testing/matricesTest.ts
@@ -1,8 +1,6 @@
+import { ElementaryMatrixRowOperations } from "../concepts/linear-algebra/matrices/ElementaryMatrixRowOperations.ts";
 import { Matrix } from "../concepts/linear-algebra/matrices/Matrix.ts";
 
 export default function main() {
-    const mtx1 = new Matrix(5, 5);
-    mtx1.print();
-    mtx1.fill((r, c) => r == c ? 1 : 0);
-    mtx1.print();
+    ElementaryMatrixRowOperations.main();
 }
