3180b544b16bb2f60c85191aca796f5e3ff9721b
Tim Leobert Villacin <timvillacin@gmail.com>
2025-04-23 22:28:43 +0800

    * Modified _testing dir

--- src/_testing/matricesTest.ts
+++ src/_testing/matricesTest.ts
@@ -1,5 +1,7 @@
 import { ElementaryMatrixRowOperations } from "../concepts/linear-algebra/matrices/operations/ElementaryMatrixRowOperations.ts";
+// import { Matrix } from "../concepts/linear-algebra/matrices/types/Matrix.ts";
 
 export default function main() {
     ElementaryMatrixRowOperations.main();
+    // new Matrix(2, 2).fill(3);
 }
